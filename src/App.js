a64793dc953f90fc0fc3c757502daff3a76e5a2b
Arigbo <arigbojesse75@gmail.com>
2022-07-13 11:56:58 -1200

    New

--- src/App.js
+++ src/App.js
@@ -1,73 +1,45 @@
 // import logo from './logo.svg';
-import { Route, Routes } from 'react-router-dom';
-import './App.scss';
-import AboutPage from './components/projects/AboutPage';
-// import './grid.scss';
-// import NavComp from './components/nav';
-// import ButtonComp from './components2/button';
-//  import HeaderComp from './components/HeaderComp';
-// import { Navs } from './components2/non-default';
-// import { Naved } from './components2/non-default';
-// import { Navie } from './components2/non-default';
-// import BodyComp from './components/BodComp';
-// import FooterComp from './components/footerComp';
-// // import ParentComp from './components/ParentComp';
- import ErrorPage from './components/pages/error';
-// import NavBar from './components/HeaderComp';
-// // import NavCom from './components/navcomp'; 
-// import FetchBox from './components/FetchBox';
-// import Logo from './components/pages/logo';
-// import Home from './components/pages/home';
-// import Contacts from './components/pages/contact';
-// import Services from './components/pages/services';
-// import Login from './components/pages/login';
-// import Welcome from './components/pages/welcome';
-// import ShareLayout from './components/pages/ShareLayout';
-// import Product from './components/pages/product';
-// import SignUp from './components/pages/signup';
-import Blog from './components/pages/Form/blog';
-import UpdateForm from './components/pages/Form/Updateform';
-import FirstForm from './components/pages/Form/firstform';
-import Singleblog from './components/pages/Form/singleblog';
-// import GridComp from './components/gridComp';
+import "./App.css";
+// import ".home.scss";
+// import "./login.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import HomeComp from "./components/pages/home";
+// import DroppleComp from "./components/pages/dropple";
+// import DriverComp from "./components/elvis/src/components/pages/driver";
+// import AboutComp from "./components/elvis/src/components/pages/about";
+// import LoginComp from "./components/projects/login";
+// import ErrorComp from "./components/elvis/src/components/pages/error";
+// import FooterComp from "./components/footer";
 
+// import DriverComp from "./components/David/src/component/Driver";
+// import HomeComp from "./projects/home";
+// import DriverComp from "./projects/Driver";
+// import DroppleComp from "./projects/dropple";
+// import ButtonComp from "./projects/button";
+// import LoginComp from "./projects/login";
+// import ErrorComp from "./projects/error";
+// import FooterComp from "./projects/footer";
+// import NavComp from "./projects/nav";
+// import PlayComp from "./projects/play";
+import AboutPage from "./projects/AboutPage";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-          <Routes>
-            {/* <Route path='/' element={<ShareLayout />} />
-            <Route index element={<Home />} />
-            <Route path='/contacts' element={<Contacts />} />
-            <Route path='/services' element={<Services />} />
-            <Route path='/product' element={<Product />} />
-            <Route path='/login' element={<Login />} />
-            
-            <Route path='/welcome' element={<Welcome />} />
-            <Route path='/logo' element={<Logo />} />
-            <Route path='/{item.id}' element={<Product />} />
-            <Route path='/signup' element={<SignUp />} /> */}
-            <Route path='/blogs' element={<Blog />} />
-            <Route path='/*' element={<ErrorPage />} />
-            <Route path='blogs/:blogId' element={<Singleblog />} />
-            <Route path='/new' element={<FirstForm />} />
-            <Route path='/edit' element={<UpdateForm />} />
-            <Route path='/aboutpage' element={<AboutPage/>}/>
-          </Routes>
-          
-               {/* <NavBar text="Login" /> */}
-        {/* <GridComp/> */}
-          {/* <FetchBox /> */}
-          {/* <UpdateForm />
-      <FirstForm />
-          <Blog />  */}
-        </div>
-      </header>
+      {/* <BrowserRouter> */}
+    <NavComp/>
+        <Routes>
+          {/* <Route path="/" index element={<HomeComp />}></Route>
+          <Route path="/dropple" element={<DroppleComp />}></Route>
+          <Route path="/driver" element={<DriverComp />}></Route> */}
+          <Route path="/aboutpage" element={<AboutPage />}></Route>
+          {/* <Route path="/login" element={<LoginComp />}></Route>
+          <Route path="*" element={<ErrorComp />}></Route> */}
+        </Routes>
+        <ButtonComp/>
+        <FooterComp />
+      {/* </BrowserRouter> */}
     </div>
-
   );
 }
-
 
 export default App;
---
+++ src/components/David/src/App.js
@@ -0,0 +1,11 @@
+import './Driver.scss';
+import DriverComp from '../../../projects/Driver';
+function App() {
+  return (
+    <div className="App">
+    <DriverComp/>
+    </div>
+  )
+}
+
+export default App;
